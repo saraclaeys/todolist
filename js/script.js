bc22cf8a4a8b7d85b485d6df04941237655fbc77
saraclaeys <sara.claeys01@gmail.com>
2020-04-17 15:37:09 +0200

    classes names

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
