1aafa8b476730d33a6d81603bf446161251dde33
saraclaeys <sara.claeys01@gmail.com>
2020-04-17 15:39:53 +0200

    date

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ const input = document.getElementById("input");
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
+
+// Show todays date
+const options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric"
+};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("nl-BE", options);
