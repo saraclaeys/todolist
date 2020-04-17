f76d10cd701c38bb4931ffc701c4c4917c3b7964
saraclaeys <sara.claeys01@gmail.com>
2020-04-17 15:48:41 +0200

    to do function

--- js/script.js
+++ js/script.js
@@ -18,3 +18,18 @@ const options = {
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("nl-BE", options);
+
+// add to do function
+function addToDo(toDo) {
+
+    const item = `<li class="item">
+                <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                <p class="text">${toDo}</p>
+                <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                </li>
+            `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
