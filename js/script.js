e395554e4d4608ebbfafc3f0b1536b44e284cda2
saraclaeys <sara.claeys01@gmail.com>
2020-04-17 16:26:11 +0200

    remove to do

--- js/script.js
+++ js/script.js
@@ -73,3 +73,10 @@ function completeToDo(element) {
 
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
+
+// remove to do
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
