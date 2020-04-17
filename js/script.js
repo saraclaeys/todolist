3ccbe9822f8300ae33d387302100dd7847280147
saraclaeys <sara.claeys01@gmail.com>
2020-04-17 15:36:52 +0200

    select elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
